92f0041aa6a08006746996aa97e29c247855eef5
root <dahirmuhammad3@gmail.com>
2018-05-03 21:19:52 +0100

    added minimum and maximum functions

--- analyse/analyse.py
+++ analyse/analyse.py
@@ -12,15 +12,26 @@ def main():
 	parser.add_argument("file", help="Specify the input fileName")
 	parser.add_argument("-P", "--percentage", help="retrieve the percentage occurence of each of the strings[per line] in the file", nargs="?", const=True)
 	parser.add_argument("-f", "--outfile", help="file to write the output [default is 'analysed.dmd']", default="analysed.dmd")
+	parser.add_argument("-M", "--max", help="return top of the number specified [eg -M 3 {top 3}]", type=int)
+	parser.add_argument("-m", "--min", help="return least of the number specified [eg -m 4 {least 4}]", type=int)
 	args = parser.parse_args()
-	analyse(args.file, args.outfile, args.percentage)
+	analyse(args.file, args.outfile, args.percentage, args.max, args.min)
 
 
-def analyse(fileName, outfile, percent=None):
+def analyse(fileName, outfile, percent=None, maxi=None, mini=None):
 	if percent:
 		percentage(fileName, outfile)
 
-def percentage(fileName, outfile):
+	if maxi:
+		percentDict = percentage(fileName, outfile, called=True)
+		maximum(percentDict, maxi)
+
+	if mini:
+		percentDict = percentage(fileName, outfile, called=True)
+		minimum(percentDict, mini)
+
+
+def percentage(fileName, outfile, called=None):
 	cleanLines = []
 	with open(fileName, "r") as f:
 		lines = f.readlines()
@@ -42,13 +53,39 @@ def percentage(fileName, outfile):
 	for item in linesDict:
 		itemValue = linesDict[item]
 		percentage = float(itemValue)/float(numberOfLines) * 100
-		percentDict[item] = str("%.2f"%percentage)+"%"
+		percentDict[item] = str("%.2f"%percentage)
+
 
 	with open(outfile, "ab") as f:
 		for item in percentDict:
-			f.write("%s ==> %s\n"%(item, percentDict[item]))
+			f.write("%s ==> %s%s\n"%(item, percentDict[item], "%"))
 
+	if called:
+		return percentDict
 
+def maximum(percentDict, num):
+	with open("top%d.dmd"%(num), "ab") as f:
+		for i in range(num):
+			maxVal = max(percentDict.values())
+			lastMaxItem = ""
+			for item in percentDict:
+				if percentDict[item] == maxVal:
+					f.write("%s ==> %s%s\n"%(item, maxVal, "%"))
+					lastMaxItem = item
+					break
+			percentDict.pop(lastMaxItem)
+
+def minimum(percentDict, num):
+	with open("least%d.dmd"%(num), "ab") as f:
+		for i in range(num):
+			minVal = min(percentDict.values())
+			lastMinItem = ""
+			for item in percentDict:
+				if percentDict[item] == minVal:
+					f.write("%s ==> %s%s\n"%(item, minVal, "%"))
+					lastMinItem = item
+					break
+			percentDict.pop(lastMinItem)
 
 if __name__ == '__main__':
 	main()
