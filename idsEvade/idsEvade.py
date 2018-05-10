55e22a58894fcd262f56a50742eff1fcabd204f1
root <dahirmuhammad3@gmail.com>
2018-05-10 17:00:57 +0100

    added function to bypass sqli WAFs

--- idsEvade/idsEvade.py
+++ idsEvade/idsEvade.py
@@ -1,18 +1,24 @@
-#CodeName: 
-#Author: 
-#Date: 
-#About: 
+#CodeName: idsEvade.py
+#Author: Dahir Muhammad Dahir
+#Date: 10th-May-2018
+#About: script to modified input payload to bypass WAFs and IDS
 
+import random
+import re
 
-
+letters = "abcdefghijklmnopqrstuvwxyz"
+controlPattern = r"\s--\s"
+pattern = r"\s"
 
 def main():
-
-
-
-
-
-
+	while(True):
+		inputPayload = raw_input("give me the payload:\n")
+		if inputPayload == "exit":
+			exit()
+		controlledPayload = re.sub(controlPattern, "ayilaR", inputPayload)
+		cleanPayload = re.sub(pattern, "/*%s*/"%("".join(random.sample(letters, 4))), controlledPayload)
+		finalPayload = re.sub("ayilaR", " -- ", cleanPayload)
+		print("Here you go:\n%s"%(finalPayload))
 
 
 
--- idsEvade/test.py
+++ idsEvade/test.py
@@ -1,1 +1,11 @@
+import re
+
+pattern = re.compile(r"\s")
+
+text = raw_input("Enter text:\n")
+print(re.sub(pattern, "/*time*/", text))
+
+
+
+
 #test
